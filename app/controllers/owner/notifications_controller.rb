7bccb66378f23bbaf1c2a7e82fee461e535b6f30
Mahfuzur Rahman <bappy.gm@gmail.com>
2025-11-20 22:30:19 +0600

    mockup - added owner notifications pages

---
+++ app/controllers/owner/notifications_controller.rb
@@ -0,0 +1,18 @@
+class Owner::NotificationsController < ApplicationController
+  layout 'owner'
+  
+  def index
+    # List all notifications for the owner
+    # Mockup: Display notification center with filters
+  end
+  
+  def show
+    # Show individual notification details
+    # Mockup: Display notification details
+  end
+  
+  def mark_as_read
+    # Mark notification as read
+    # Mockup: Update notification status
+  end
+end
