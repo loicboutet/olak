f1cf0eb6fce636f2488108195ce0ab020100c6a6
Mahfuzur Rahman <bappy.gm@gmail.com>
2025-11-20 09:36:27 +0600

    mockup - added admin applications pages

---
+++ app/controllers/admin/applications_controller.rb
@@ -0,0 +1,18 @@
+class Admin::ApplicationsController < ApplicationController
+  layout 'admin'
+  
+  def index
+    # Controller logic will be implemented later
+    # This is a mockup controller
+  end
+  
+  def show
+    # Controller logic will be implemented later
+    # This is a mockup controller
+  end
+  
+  def propose_to_owner
+    # Controller logic will be implemented later
+    # This is a mockup controller
+  end
+end
