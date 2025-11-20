aa2bb507f34df5b1a4fa6b6ac33ed8ec76323e4d
Mahfuzur Rahman <bappy.gm@gmail.com>
2025-11-20 10:45:41 +0600

    mockup - added admin rattings pages

---
+++ app/controllers/admin/ratings_controller.rb
@@ -0,0 +1,28 @@
+class Admin::RatingsController < ApplicationController
+  layout 'admin'
+  
+  def new
+    # Controller logic will be implemented later
+    # This is a mockup controller
+  end
+  
+  def create
+    # Controller logic will be implemented later
+    # This is a mockup controller
+  end
+  
+  def show
+    # Controller logic will be implemented later
+    # This is a mockup controller
+  end
+  
+  def edit
+    # Controller logic will be implemented later
+    # This is a mockup controller
+  end
+  
+  def update
+    # Controller logic will be implemented later
+    # This is a mockup controller
+  end
+end
